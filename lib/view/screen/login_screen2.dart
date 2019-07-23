7850c7539202bb79f084b693b86f331587b1e5df
mobiledevsigma <mobiledevsigma>
2019-07-23 15:39:54 +0700

    baru

---
+++ lib/view/screen/login_screen2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen2 extends StatefulWidget {
+  @override
+  _LoginScreenState2 createState() => _LoginScreenState2();
+}
+
+class _LoginScreenState2 extends State<LoginScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+    return null;
+  }
+
+}
